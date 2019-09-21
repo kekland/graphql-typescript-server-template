3a0abac729b37ab6d07e7e337e6e4e4d33fb447a
kekland <kk.erzhan@gmail.com>
2019-09-21 16:20:03 +0600

    adding flag checking

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import "reflect-metadata"
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
 import { RecipeResolver } from './recipes/recipe';
-declare const module: any;
 
 const PORT = process.env.PORT || 8080;
 
 async function bootstrap() {
-  // Add resolvers here
   const schema = await buildSchema({
+    // IMPORTANT: Add resolvers here
     resolvers: [RecipeResolver],
   });
 
@@ -19,11 +18,6 @@ async function bootstrap() {
 
   const { url } = await server.listen(PORT);
   console.log(`Server is running, GraphQL Playground available at ${url}`);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => server.stop())
-  }
 }
 
 bootstrap()
